0e494c9c02875a58f6a9bf71ad58747622d48a3e
unknown <kollur@KOLLUR1.asiapacific.hpqcorp.net>
2014-07-14 11:29:10 +0530

    message

--- sample.py
+++ sample.py
@@ -1,7 +1,3 @@
 sample test git file
 something changed
-<<<<<<< HEAD
-something add local changes
-=======
-something changed on master
->>>>>>> 92928d95e984129fc6eb5572dd584dcfa1369ca9
+something changed on master add local changes
